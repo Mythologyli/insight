39b1699662e1bca56b8d7e5f1cf6a637477b2d42
Myth <lxbpxylps@126.com>
2021-12-14 23:43:23 +0800

    feat: add RaidTrigger detection

--- src/main/java/cc/akashic/insight/EventBroadcastListener.java
+++ src/main/java/cc/akashic/insight/EventBroadcastListener.java
@@ -1,9 +1,11 @@
 package cc.akashic.insight;
 
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.raid.RaidTriggerEvent;
 
 public final class EventBroadcastListener implements Listener {
     @EventHandler
@@ -11,5 +13,13 @@ public final class EventBroadcastListener implements Listener {
         Player player = event.getEntity();
 
         Log.info("EVENT|DEAD|" + player.getName() + "|" + event.getDeathMessage() + "|" + player.getLevel());
+    }
+
+    @EventHandler
+    public void onRaidTrigger(RaidTriggerEvent event) {
+        Player player = event.getPlayer();
+        Location location = player.getLocation();
+
+        Log.info("EVENT|RAIDTRIGGER|" + player.getName() + "|" + location.getX() + "," + location.getY() + "," + location.getZ());
     }
 }
