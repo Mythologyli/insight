5cb7000caf615c9045c0ec6115554b53c7ef7900
Myth <lxbpxylps@126.com>
2022-06-14 10:54:20 +0800

    fix: spectator now can keep when spectated player change world

--- src/main/java/cc/akashic/insight/SpectatorKeeper.java
+++ src/main/java/cc/akashic/insight/SpectatorKeeper.java
@@ -8,6 +8,7 @@ import java.util.HashMap;
 
 public final class SpectatorKeeper {
     static private final HashMap<Player, Player> playerSpectatePlayer = new HashMap<>();
+    static private final HashMap<Player, Boolean> spectatorLock = new HashMap<>();
     static private int taskID;
 
     public static void setPlayerSpectatePlayer(Player playerSpectator, Player playerSpectated) {
@@ -16,10 +17,12 @@ public final class SpectatorKeeper {
         }
 
         playerSpectatePlayer.put(playerSpectator, playerSpectated);
+        spectatorLock.put(playerSpectator, false);
     }
 
     public static void removePlayerSpectatePlayer(Player playerSpectator) {
         playerSpectatePlayer.remove(playerSpectator);
+        spectatorLock.remove(playerSpectator);
 
         if (playerSpectatePlayer.isEmpty()) {
             Bukkit.getScheduler().cancelTask(taskID);
@@ -31,13 +34,24 @@ public final class SpectatorKeeper {
             Player playerSpectator = entry.getKey();
             Player playerSpectated = entry.getValue();
 
+            if (spectatorLock.get(playerSpectator)) {
+                continue;
+            }
+
             if (playerSpectator.getGameMode() != GameMode.SPECTATOR || !playerSpectator.isOnline() || !playerSpectated.isOnline()) {
                 playerSpectatePlayer.remove(playerSpectator);
+                spectatorLock.remove(playerSpectator);
                 continue;
             }
 
             if (playerSpectator.getSpectatorTarget() != playerSpectated || playerSpectator.getWorld() != playerSpectated.getWorld() || playerSpectator.getLocation().distance(playerSpectated.getLocation()) > 2.0) {
-                Bukkit.getServer().dispatchCommand(playerSpectator, "spectate " + playerSpectated.getName());
+                spectatorLock.put(playerSpectator, true);
+                Bukkit.getServer().dispatchCommand(playerSpectator, "spectate");
+
+                Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(Insight.instance, () -> {
+                    Bukkit.getServer().dispatchCommand(playerSpectator, "spectate " + playerSpectated.getName());
+                    spectatorLock.put(playerSpectator, false);
+                }, 100L);
             }
         }
     }
