e12f41ef7f0058e6888c356a6f5db7ec7f03eadf
Myth <lxbpxylps@126.com>
2023-03-09 20:16:15 +0800

    feat: translation

--- src/main/java/cc/akashic/insight/EventBroadcastListener.java
+++ src/main/java/cc/akashic/insight/EventBroadcastListener.java
@@ -1,18 +1,59 @@
 package cc.akashic.insight;
 
+import net.kyori.adventure.text.TranslatableComponent;
+import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerAdvancementDoneEvent;
 import org.bukkit.event.raid.RaidTriggerEvent;
 
 public final class EventBroadcastListener implements Listener {
     @EventHandler
     public void onPlayerDeath(PlayerDeathEvent event) {
         Player player = event.getEntity();
+        var deathMessage = event.deathMessage();
 
-        Log.info("EVENT|DEAD|" + player.getName() + "|" + event.deathMessage() + "|" + player.getLevel());
+        StringBuilder componentString = new StringBuilder();
+
+        if (deathMessage instanceof TranslatableComponent) {
+            var args = ((TranslatableComponent) deathMessage).args();
+
+            componentString.append("K,").append(((TranslatableComponent) deathMessage).key());
+            for (var arg : args) {
+                if (arg instanceof TranslatableComponent) {
+                    componentString.append(";K,").append(((TranslatableComponent) arg).key());
+                } else {
+                    componentString.append(";T,").append(PlainTextComponentSerializer.plainText().serialize(arg));
+                }
+            }
+        } else {
+            assert deathMessage != null;
+            componentString.append("T,").append(PlainTextComponentSerializer.plainText().serialize(deathMessage));
+        }
+
+        Log.info("EVENT|DEAD|" + player.getName() + "|" + componentString + "|" + player.getLevel());
+    }
+
+    @EventHandler
+    public void onPlayerAdvancementDone(PlayerAdvancementDoneEvent event) {
+        Player player = event.getPlayer();
+        var advancementDisplay = event.getAdvancement().displayName();
+        String advancementString = "";
+
+        if (advancementDisplay instanceof TranslatableComponent) {
+            var args = ((TranslatableComponent) advancementDisplay).args();
+
+            for (var arg : args) {
+                if (arg instanceof TranslatableComponent) {
+                    advancementString = ((TranslatableComponent) arg).key();
+                }
+            }
+        }
+
+        Log.info("EVENT|ADVANCEMENT|" + player.getName() + "|" + advancementString);
     }
 
     @EventHandler
