26c0931c5184499c57c376b795d8122549eb1615
Myth <myth@myth.cx>
2024-06-22 23:27:42 +0800

    feat: item translator

--- src/main/java/cc/akashic/insight/utils/ItemsViewer.java
+++ src/main/java/cc/akashic/insight/utils/ItemsViewer.java
@@ -1,6 +1,9 @@
 package cc.akashic.insight.utils;
 
 import cc.akashic.insight.Insight;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
@@ -14,6 +17,8 @@ import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryView;
 import org.bukkit.inventory.ItemStack;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.HashSet;
 import java.util.Map;
 
@@ -27,6 +32,18 @@ public final class ItemsViewer {
      */
     public static void printToConsole(ItemStack[] items) {
         boolean isEmpty = true;
+        boolean doTranslate = false;
+        JsonObject transJsonObject = null;
+
+        // Check if minecraft_lang.json exists
+        try {
+            FileReader reader = new FileReader(Insight.dataFolder + "/minecraft_lang.json");
+            doTranslate = true;
+            JsonParser parser = new JsonParser();
+            JsonElement jsonElement = parser.parse(reader);
+            transJsonObject = jsonElement.getAsJsonObject();
+        } catch (FileNotFoundException ignored) {
+        }
 
         for (ItemStack item : items) {
             if (item != null) {
@@ -36,12 +53,34 @@ public final class ItemsViewer {
                 }
 
                 isEmpty = false;
+
+                // Translate the material name
+                String materialName = material.toString();
+                if (doTranslate) {
+                    try {
+                        materialName = transJsonObject.get(("item.minecraft." + material.toString().toLowerCase())).getAsString();
+                    } catch (NullPointerException ignoredItem) {
+                        try {
+                            materialName = transJsonObject.get(("block.minecraft." + material.toString().toLowerCase())).getAsString();
+                        } catch (NullPointerException ignoredBlock) {
+                        }
+                    }
+                }
+
                 Map<Enchantment, Integer> enchantments = item.getEnchantments();
 
-                StringBuilder message = new StringBuilder(material + " x" + item.getAmount() + " ");
+                StringBuilder message = new StringBuilder(materialName + " x" + item.getAmount() + " ");
 
                 for (Map.Entry<Enchantment, Integer> entry : enchantments.entrySet()) {
-                    message.append("| ").append(entry.getKey().getKey()).append(" ").append(entry.getValue()).append(" ");
+                    // Translate the enchantment name
+                    String enchantmentName = entry.getKey().getKey().toString();
+                    if (doTranslate) {
+                        try {
+                            enchantmentName = transJsonObject.get(("enchantment." + entry.getKey().getKey().toString().replace(":", "."))).getAsString();
+                        } catch (NullPointerException ignored) {
+                        }
+                    }
+                    message.append("| ").append(enchantmentName).append(" ").append(entry.getValue()).append(" ");
                 }
 
                 Bukkit.getLogger().info(message.toString());
