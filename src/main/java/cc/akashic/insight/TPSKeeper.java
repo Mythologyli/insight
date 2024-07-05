7eef2b276036820420fff21a08fd43eac59afbbe
Myth <myth@myth.cx>
2024-07-05 17:10:21 +0800

    feat: kick afk player in tps keep mode

--- src/main/java/cc/akashic/insight/TPSKeeper.java
+++ src/main/java/cc/akashic/insight/TPSKeeper.java
@@ -7,7 +7,11 @@ import me.lucko.spark.api.statistic.types.DoubleStatistic;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
-import org.bukkit.*;
+import org.bukkit.Bukkit;
+import org.bukkit.Difficulty;
+import org.bukkit.GameRule;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
 import org.bukkit.entity.SpawnCategory;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -18,6 +22,8 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+
+import static cc.akashic.insight.AFK.AFKPlayerSet;
 
 public final class TPSKeeper {
     private static Spark spark;
@@ -86,6 +92,11 @@ public final class TPSKeeper {
 
         // Change monster spawn limit.
         world.setSpawnLimit(SpawnCategory.MONSTER, originMonsterSpawnLimit / 2);
+
+        // Kick all AFK players.
+        for (Player player : AFKPlayerSet) {
+            player.kickPlayer("Kicked by TPS Keep Mode.");
+        }
     }
 
     public static void disableTPSKeepMode() {
